fbdbb03361143c19224b5e5cad1aebd6389a1c81
Ulvi Valiyev <heli211@live.ru>
2021-01-27 11:28:21 -0800

    last commit

--- Bayram IOS App/Model/Login/LoginNetworkManager.swift
+++ Bayram IOS App/Model/Login/LoginNetworkManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 import Alamofire
 import SwiftyJSON
-
+//
 struct LoginNetworkManager {
     
     func loginRequest(endpoint: String, parameters: [String: String], completetion: @escaping (Result<JSON, Error>) -> Void) {
